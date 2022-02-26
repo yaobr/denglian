50f9cde5064f60b256057a287ebe7e755eec7287
oxbray <birui.yao@gmail.com>
2022-02-26 19:37:15 +0800

    w1_1
    第一周第一节课作业

---
+++ w1/w1_1/counter.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.0;
+
+contract Counter {
+    uint public counter;
+
+    constructor() {
+        counter = 0;
+    }
+
+    function count() public {
+        counter = counter + 1;
+    }
+}
